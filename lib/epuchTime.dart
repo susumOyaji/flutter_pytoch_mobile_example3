d199e20daa737ff0bac55e33be3fb1900ae30fb8
susumOyaji <sumitomo0210@gmail.com>
2022-06-23 09:34:30 +0900

    up

--- lib/epuchTime.dart
+++ lib/epuchTime.dart
@@ -86,12 +86,50 @@ class _MyHomePageState extends State<MyHomePage> {
     String csv = const ListToCsvConverter().convert(rowsAsListOfValues);
 
     //final result = await http.get(url);
-    String response_j;
-    setState(() {
-      Map<String, dynamic> jsonData = json.decode(response
-          .body); //['Date','Open','High,'Low','Close','Adj Close',Volume'];
-    });
+    //String response_j;
+    //setState(() {
+    //  Map<String, dynamic> jsonData = json.decode(response
+    //      .body); //['Date','Open','High,'Low','Close','Adj Close',Volume'];
+    //});
   }
+
+/*
+''学習データ(train_data)を正規化'''
+#特徴量を時系列にグラフ表示に示した通り、割とデータには開きがあります。なので、最小値と最大値を決めた正規化を学習データに行います。
+
+# データセットの正規化を行う。最小値0と最大値1の範囲で行う。
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0, 1))
+train_data_normalized = scaler.fit_transform(train_data)
+
+
+'''Tensor型に変換'''
+#PyTorchを使うのでTensor型にします。
+train_data_normalized = torch.FloatTensor(train_data_normalized)
+
+
+'''予測のためのデータ準備'''
+#学習によってパラメータが決定したので、このモデルを使って予測してみます。
+# そのために学習データとテストデータに分割するにて分割したtest_dataをデータを正規化で作成したscalerを用いて変換します。
+# train_dataの正規化で取得した統計情報をtest_data正規化時にも使って変換します。(scaler.fit_transform)
+#また、pred_daysは予測する日数です。本記事では、30日分のtest_dataとこれから予測する30日分を比較してみます。
+#さらに、test_inputsというリストを用意します。
+# test_inputsには学習データを正規化で正規化した学習データ(train_data_normalized)の直近seq_length(=30)日分を入れます。
+# これは予測時に使います。
+
+# 予測する日数
+pred_days = 30
+
+# テストデータの正規化を行う。最小値0と最大値1の範囲で行ってTensor型に変換する。
+test_data_normalized = scaler.fit_transform(test_data)
+test_data_normalized = torch.FloatTensor(test_data_normalized)
+
+# 予測するためのデータの最初のseq_length分はtrain_dataを使う
+test_inputs = train_data_normalized[-seq_length:].tolist()
+
+
+
+*/
 
   void _incrementCounter() {
     setState(() {
